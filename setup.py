bef3b2a02d9707db9b11000c8b5089c2ec475eac
cxw1219 <drbencadet@gmail.com>
2024-12-14 17:59:36 +0000

    Add pyproject.toml and update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="squid",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'oandapyV20>=0.7.2',
         'pandas>=1.3.0',
